1472ab0d06368da298316ea31a1351f7c43e2ba1
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-01 14:07:50 +0200

    Refactor

--- openmp_petle_simple.c
+++ openmp_petle_simple.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define WYMIAR 18
+#define SIZE 18
 #define THREADS_AMOUNT 4
 
 int main() {
-    double a[WYMIAR];
+    double a[SIZE];
 
     omp_set_num_threads(THREADS_AMOUNT);
 
 #pragma omp parallel for default(none) shared(a)
-    for (int i = 0; i < WYMIAR; i++) a[i] = 1.02 * i;
+    for (int i = 0; i < SIZE; i++) a[i] = 1.02 * i;
 
-    // pętla sekwencyjna
+    // sequential loop
     double suma = 0.0;
-    for (int i = 0; i < WYMIAR; i++) {
+    for (int i = 0; i < SIZE; i++) {
         suma += a[i];
     }
 
-    printf("Suma wyrazow tablicy: %lf\n", suma);
+    printf("The sum of the array's terms: %lf\n", suma);
 
-    // pętla do modyfikacji - docelowo równoległa w OpenMP
+    // parallel loop
 
     double suma_parallel = 0.0;
 #pragma omp parallel for default(none) reduction(+:suma_parallel) shared(a) ordered
 
-    for (int i = 0; i < WYMIAR; i++) {
-        int id_w = omp_get_thread_num();
+    for (int i = 0; i < SIZE; i++) {
+        int threadId = omp_get_thread_num();
         suma_parallel += a[i];
 
 #pragma omp ordered
-        printf("a[%2d]->W_%1d  \n", i, id_w);
+        printf("a[%2d]->W_%1d  \n", i, threadId);
     }
 
-    printf("\nSuma wyrazow tablicy rownolegle (z klauzula - schedule(static): %lf\n\n", suma_parallel);
+    printf("\nSum of array terms in parallel (with clause - schedule(static): %lf\n\n", suma_parallel);
 
-    // pętla do modyfikacji - docelowo równoległa w OpenMP
     suma_parallel = 0.0;
 
 #pragma omp parallel for schedule(dynamic) default(none) ordered shared(a) reduction(+ : suma_parallel)
-    for (int i = 0; i < WYMIAR; i++) {
+    for (int i = 0; i < SIZE; i++) {
         int id_w = omp_get_thread_num();
         suma_parallel += a[i];
 
@@ -48,7 +47,7 @@ int main() {
     }
 
     if (suma - suma_parallel > 1e-9 || suma - suma_parallel < -1e-9)
-        printf("suma policzona nie poprawnie!\nsekwencyjnie: %lf\nrownolegle: %lf\n", suma, suma_parallel);
+        printf("Sum counted incorrectly!\nsequential: %lf\nparallel: %lf\n", suma, suma_parallel);
 
-    printf("\nSuma wyrazow tablicy rownolegle (z klauzula - schedule(dynamic): %lf\n", suma_parallel);
+    printf("\nSum of array terms in parallel (with clause - schedule(dynamic): %lf\n", suma_parallel);
 }
